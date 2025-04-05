ed294c2e24ca7768bf5acbc521bf268a626a4ae3
NureAlam68 <nurealam151068@gmail.com>
2025-04-05 10:16:02 +0600

    connect mongoose

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const todoHandler = require("./todoHandler/todoHandler")
 
-app.use(express.json())
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/todos', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+// application routes
+app.use("/todo", todoHandler);
 
 app.get('/', (req, res) => {
-  res.send('Mongoose CRUD API')
-})
+  res.send('Mongoose CRUD API');
+});
 
 app.listen(port, () => {
-  console.log(`Mongoose app listening on port ${port}`)
-})
+  console.log(`🚀 Server is running on port: ${port}`);
+});
---
+++ todoHandler/todoHandler.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+// get all todos
+router.get("/", async(req, res) => {
+
+})
+
+// get todo by id
+router.get("/:id", async(req, res) => {
+
+})
+
+// create todo
+router.post("/", async(req, res) => {
+
+})
+
+// create multiple todos
+router.post("/all", async(req, res) => {
+
+})
+
+// update todo by id
+router.put("/:id", async(req, res) => {
+
+})
+
+// delete todo by id
+router.delete("/:id", async(req, res) => {
+
+})
+
+module.exports = router;
